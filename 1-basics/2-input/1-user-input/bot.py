6f8c0a5590b67ddd40235f631e6721e9c8e0ce89
Joe Bailey <4bailj07@solent.ac.uk>
2018-09-27 17:02:33 +0100

    Update bot.py
    Added code to override the default separator

--- 1-basics/2-input/1-user-input/bot.py
+++ 1-basics/2-input/1-user-input/bot.py
@@ -1,10 +1,26 @@
 # Read some user input
 print("Please type your name")
 name = input()
-print("Hello", name)
+print("Hello", name + ".")
 print("Please type your last name")
 lname = input()
-print("Hello", name, lname)
+print("Hello", name, lname + ".")
 print("Please type your profession")
 prof = input()
-print("Hello", name, lname, "You are a", prof)
+print("Hello", name, lname + ".", "You are a", prof + ".")
+
+# Alternatively you can do the following
+print("Please type your name")
+name = input()
+print("Hello ", name, ".", sep="")
+print("Please type your last name")
+lname = input()
+print("Hello ", name, " ", lname, ".", sep="")
+print("Please type your profession")
+prof = input()
+print("Hello ", name, " ", lname, ". ", "You are a ", prof, ".", sep="")
+
+# For fun
+print("Please type your name")
+name = input()
+print("Hello ", name, ".", sep=":)")
