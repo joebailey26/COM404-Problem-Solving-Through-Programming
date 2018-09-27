8be58975ea08b10abb86c1bf727681bdee05f62f
Joe Bailey <4bailj07@solent.ac.uk>
2018-09-27 16:52:17 +0100

    Create bot.py
    Added code to read user input

---
+++ 1-basics/2-input/1-user-input/bot.py
@@ -0,0 +1,10 @@
+# Read some user input
+print("Please type your name")
+name = input()
+print("Hello", name)
+print("Please type your last name")
+lname = input()
+print("Hello", name, lname)
+print("Please type your profession")
+prof = input()
+print("Hello", name, lname, "You are a", prof)
