67d6bd2b76163b94320bc378f9c5b81fad1fc9c3
Joe Bailey <4bailj07@solent.ac.uk>
2018-10-04 15:44:53 +0100

    Create bot.py
    Example of IF statement

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,11 @@
+#Read direction from user
+print("What direction do you want to move?")
+print("1) Forward")
+print("2) Backwards")
+print("3) Left")
+print("4) Right")
+
+direction = int(input())
+
+if direction == 1:
+  print("I am moving up")
