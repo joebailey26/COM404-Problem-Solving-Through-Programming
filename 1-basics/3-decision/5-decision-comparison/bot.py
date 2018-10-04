86154a3d226b80b5063c8c079bfcc516f954957e
Joe Bailey <4bailj07@solent.ac.uk>
2018-10-04 16:21:37 +0100

    Create bot.py
    Created code to compare two input numbers

---
+++ 1-basics/3-decision/5-decision-comparison/bot.py
@@ -0,0 +1,12 @@
+print("Please enter the first number")
+first_number = int(input())
+
+print("Please enter the second number")
+second_number = int(input())
+
+if first_number > second_number:
+  print("The first number is the largest")
+elif first_number < second_number:
+  print("The second number is the largest")
+else:
+  print("Both numbers are equal")
