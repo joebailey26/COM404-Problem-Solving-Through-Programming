3ef34f7d4e5b35aef66cad6660cab0c7319b10d7
Joe Bailey <4bailj07@solent.ac.uk>
2018-10-11 16:24:51 +0100

    Update bot.py

--- 1-basics/4-repitition/4-sum-100/bot.py
+++ 1-basics/4-repitition/4-sum-100/bot.py
@@ -3,3 +3,13 @@ print("The sum of the first 100 numbers is")
 numbers = sum(range(0,101))
 
 print(numbers)
+
+#alternate method 
+
+count = 0
+sum_result = 0
+
+for count in range (0,101,1):
+  sum_result = count +sum_result
+
+print("The sum of the first 100 numbers is:", sum_result)
