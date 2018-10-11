57abd05fa49c2f6c611dc1e6c965ee4bfee2dafe
Joe Bailey <4bailj07@solent.ac.uk>
2018-10-11 15:58:52 +0100

    Update bot.py

--- 1-basics/4-repitition/6-factorial/bot.py
+++ 1-basics/4-repitition/6-factorial/bot.py
@@ -4,3 +4,15 @@ print("What is your number?")
 number = int(input())
 factorial = math.factorial(number)
 print(factorial)
+
+#alternate method
+
+print("What is your number?")
+n = int(input())
+fact = 1
+  
+for i in range(1,n+1): 
+    fact = fact * i 
+      
+print ("The factorial of", n, "is: ",end="") 
+print (fact) 
