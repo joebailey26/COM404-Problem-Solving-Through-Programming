e270f653c26c8983d2d80fc02ce2d951da6119a0
Joe Bailey <4bailj07@solent.ac.uk>
2018-10-18 14:41:51 +0100

    Create bot.py
    Added code to identify Cinderella with a user-defined function.

---
+++ 1-basics/5-functions/3-cinderella/bot.py
@@ -0,0 +1,10 @@
+def try_glass_slipper_on(person):
+  if person == "Eldest step sister":
+    print("You are not my princess. Your feet are too big!")
+  elif person == "Youngest step sister":
+    print("You are not my princess. Your feet are too small!")
+  elif person == "Cinderella":
+    print("I found my princess!")
+try_glass_slipper_on("Eldest step sister")
+try_glass_slipper_on("Youngest step sister")
+try_glass_slipper_on("Cinderella")
